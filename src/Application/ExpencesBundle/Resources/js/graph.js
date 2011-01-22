badf0fd097f3ba8436b33048687f8893e0dfdf14
Wojciech Sznapka <wojciech@sznapka.pl>
2011-01-22 23:49:03 +0100

    Added series label into hover tooltip on the graph

--- src/Application/ExpencesBundle/Resources/js/graph.js
+++ src/Application/ExpencesBundle/Resources/js/graph.js
@@ -17,7 +17,12 @@ if (typeof(flotData) != "undefined") {
           $("#" + _tooltipId).remove();
           var y = item.datapoint[1].toFixed(2);
           var date = new Date(item.datapoint[0]);
-          var contents = "<strong>" + date.getFullYear() + "-" + (date.getMonth() + 1) + "</strong><br />" + y;
+          if (item.series.label != "") {
+            var contents = "<strong>" + item.series.label + "</strong><br />";
+          } else {
+            var contents = "";
+          }
+          contents += "<em>" + date.getFullYear() + "-" + (date.getMonth() + 1) + "</em><br />" + y;
           $('<div id="'+ _tooltipId +'">' + contents + '</div>').css( {
               top: item.pageY + 10,
               left: item.pageX + 10,
