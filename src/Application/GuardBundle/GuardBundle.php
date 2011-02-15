fe4d2b7ad86c4407b5fb6c66e0440ef67a68e547
Wojciech Sznapka <wojciech@sznapka.pl>
2011-02-16 00:36:07 +0100

    Fixed bundles classes and added submodules

--- src/Application/GuardBundle/GuardBundle.php
+++ src/Application/GuardBundle/GuardBundle.php
@@ -8,4 +8,13 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class GuardBundle extends Bundle
 {
+  public function getNamespace()
+  {
+    return __NAMESPACE__;
+  }
+
+  public function getPath()
+  {
+    return __DIR__;
+  }
 }
