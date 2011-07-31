4af60a3b1ba0f306acfd38ea57c9a7e9d4e8c07e
Wojciech Sznapka <wojciech@sznapka.pl>
2011-07-31 13:25:24 +0200

    Removed whole Symfony project and moved ExpencesBundle contents into root, so now repository consist only bundle itself

--- web/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__.'/../app/autoload.php';
-require_once __DIR__.'/../app/AppKernel.php';
-
-use Symfony\Component\HttpFoundation\Request;
-
-$kernel = new AppKernel('prod', true);
-$kernel->handle(Request::createFromGlobals())->send();
